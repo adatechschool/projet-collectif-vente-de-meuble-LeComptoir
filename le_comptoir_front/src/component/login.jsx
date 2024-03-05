473e97a8198d1d5c717aed4f87d6b72913abf2db
maxlebro <z.joriot@gamil.com>
2024-03-05 14:14:29 +0100

    apparition du login

---
+++ le_comptoir_front/src/component/login.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import { Box, Button, FormControl, FormLabel, Input, Stack } from '@chakra-ui/react';
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        // Logique de connexion ici
+    };
+
+    return (
+        <Box maxW="md" mx="auto" mt={8} p={4}>
+            <FormControl>
+                <FormLabel>Email</FormLabel>
+                <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+            </FormControl>
+            <FormControl mt={4}>
+                <FormLabel>Mot de passe</FormLabel>
+                <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </FormControl>
+            <Stack mt={4} direction="row" spacing={4}>
+                <Button colorScheme="blue" onClick={handleLogin}>Se connecter</Button>
+                <Button variant="outline">Mot de passe oublié</Button>
+            </Stack>
+        </Box>
+    );
+};
+
+export default Login;
