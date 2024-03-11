9a4d2c95f241b9344b78e1464e6edfd0eeab4d4a
TimotheeRoy <timroy93100@gmail.com>
2024-03-11 09:52:09 +0100

    add logo

--- le_comptoir_front/src/component/header.jsx
+++ le_comptoir_front/src/component/header.jsx
@@ -1,11 +1,13 @@
-import { Box, Heading, Input, Button ,} from "@chakra-ui/react"
+import { Box, Heading, Input, Button , Image} from "@chakra-ui/react"
 import { Link } from "react-router-dom";
 
 function header() {
- 
+
     return (
         <Box display={"flex"} justifyContent={"space-between"} width={"100%"} paddingX={"4rem"}>
-            <Heading as="h1" size="2xl">logo</Heading>
+            <Heading as="h1" size="2xl"> 
+            <Image src={"./image/Logo.jpg"} alt={"logo"} />
+            </Heading>
             <Box>
             <Heading paddingBottom={"2rem"} as="h1" size="2xl">Le Comptoir</Heading>
             <Input maxW={"15rem"} placeholder="Search" />
