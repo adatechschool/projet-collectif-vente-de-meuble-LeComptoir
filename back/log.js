e89cd61d8da2cd00975ad099df9cd9ba93e4b78c
TimotheeRoy <timroy93100@gmail.com>
2024-03-12 10:36:28 +0100

    add .env in /back

--- back/log.js
+++ back/log.js
@@ -1,9 +1,9 @@
-const { createClient } = require("@supabase/supabase-js");
+require("dotenv").config();
 
 // URL et clé d'API Supabase
+const { createClient } = require("@supabase/supabase-js");
 const supabaseUrl = "https://aeyokqlrkmsdphrayffh.supabase.co";
-const supabaseKey =
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFleW9rcWxya21zZHBocmF5ZmZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDk1NjA1NjIsImV4cCI6MjAyNTEzNjU2Mn0.81oa_X285MaatrDWu8rwUvf1-7fGRK_Y3RGZHke7Xuk";
+const supabaseKey = process.env.API_KEY;
 
 // Création du client Supabase
 const supabase = createClient(supabaseUrl, supabaseKey);
