b262f292e763ecba968735433f2bc94188e63c82
TimotheeRoy <timroy93100@gmail.com>
2024-03-12 10:52:34 +0100

    delete get with id

--- back/index.js
+++ back/index.js
@@ -3,36 +3,29 @@ const express = require("express");
 const { json } = require("express");
 const app = express();
 const port = 3000;
-const cors = require('cors')
+const cors = require("cors");
 
 const corsOptions = {
-    origin : [
-        "http://localhost:5173"
-    ],
-    optionSuccesStatus : 200
-}
-app.use(cors(corsOptions))
+    origin: ["http://localhost:5173"],
+    optionSuccesStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(json());
 
 // import des fonctions de requetes
-const { fetchMeublesHomePage, fetchMeubleDetails, fetchMeubleAdmin } = require("./supabase");
+const { fetchMeublesHomePage, fetchMeubleAdmin } = require("./supabase");
 
 // endpoints
-app.get("/meubles", async (req, res) => {       // req -> parametre de la route ; res -> reponse de la route
+app.get("/meubles", async (req, res) => {
+    // req -> parametre de la route ; res -> reponse de la route
     const data = await fetchMeublesHomePage();
-    res.send(data);   // equivalnt à un return data
+    res.send(data); // equivalnt à un return data
 });
 
-app.get("/meubles/:id", async (req, res) => {
-    const { id } = req.params;      //on récup le parametre qui correspond à id dans le chemin 
-    const data = await fetchMeubleDetails(id);
+app.get("/admin", async (req, res) => {
+    const data = await fetchMeubleAdmin();
     res.send(data);
 });
-
-app.get("/admin", async (req , res) =>{
-    const data = await fetchMeubleAdmin()
-    res.send(data)
-} )
 
 app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur le port ${port}`);
--- le_comptoir_front/src/component/login.jsx
+++ le_comptoir_front/src/component/login.jsx
@@ -26,16 +26,20 @@ const Login = () => {
     return (
         <Box maxW="md" mx="auto" mt={8} p={4}>
             
-            <Box><Heading paddingBottom={"2rem"} as="h1" size="2xl">Le Comptoir</Heading>
+            <Box>
+                <Heading paddingBottom={"2rem"} as="h1" size="2xl">Le Comptoir</Heading>
             </Box>
+            
             <FormControl>
                 <FormLabel>Email</FormLabel>
                 <Input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
             </FormControl>
+            
             <FormControl mt={4}>
                 <FormLabel>Mot de passe</FormLabel>
                 <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
             </FormControl>
+            
             <Stack mt={4} direction="row" spacing={4}>
                 <Button colorScheme="blue" onClick={handleLogin}>Se connecter</Button>
                 <Button variant="outline">Mot de passe oublié</Button>
