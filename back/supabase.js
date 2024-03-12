2be4a726c9ffe8df53183662d941c8a189f11764
maxlebro <z.joriot@gamil.com>
2024-03-12 14:39:45 +0100

    mise en place route delete et update

--- back/supabase.js
+++ back/supabase.js
@@ -8,70 +8,81 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 // requests functions
 const fetchMeublesHomePage = async () => {
-    try {
-        let { data: meubles, error } = await supabase //api de supabase pour fetch les données d'une table
-            .from("meubles")
-            .select("id, nom, image, type, prix, description")
-            .eq("statut", "Disponible");
-        if (error) {
-            throw error;
-        }
-        return meubles;
-    } catch (error) {
-        console.error(
-            "Erreur lors de la récupération des meubles:",
-            error.message
-        );
-        throw error;
+  try {
+    let { data: meubles, error } = await supabase //api de supabase pour fetch les données d'une table
+      .from("meubles")
+      .select("id, nom, image, type, prix, description")
+      .eq("statut", "Disponible");
+    if (error) {
+      throw error;
     }
+    return meubles;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des meubles:", error.message);
+    throw error;
+  }
 };
 
 const fetchMeubleAdmin = async () => {
-    try {
-        let { data: meubles, error } = await supabase
-            .from("meubles")
-            .select("id, nom, statut, prix");
-        if (error) {
-            throw error;
-        }
-        return meubles;
-    } catch (error) {
-        console.error(
-            "Erreur lors de la récupération des meubles:",
-            error.message
-        );
-        throw error;
+  try {
+    let { data: meubles, error } = await supabase
+      .from("meubles")
+      .select("id, nom, statut, prix");
+    if (error) {
+      throw error;
     }
+    return meubles;
+  } catch (error) {
+    console.error("Erreur lors de la récupération des meubles:", error.message);
+    throw error;
+  }
 };
 
 const signInUser = async (email, password) => {
-    try {
-        // Récupérer les informations de l'utilisateur à partir de la base de données
-        const { data: users, error } = await supabase
-            .from("admin")
-            .select("*")
-            .eq("email", email)
-            .eq("password", password)
-            .limit(1);
+  try {
+    // Récupérer les informations de l'utilisateur à partir de la base de données
+    const { data: users, error } = await supabase
+      .from("admin")
+      .select("*")
+      .eq("email", email)
+      .eq("password", password)
+      .limit(1);
 
-        // Vérifier s'il y a une erreur
-        if (error) {
-            throw error;
-        }
-
-        // Vérifier si l'utilisateur existe
-        if (users.length === 0) {
-            console.log("Utilisateur non trouvé");
-            return { error: "Utilisateur non trouvé" };
-        }
-
-        // Si l'utilisateur est trouvé, renvoyer l'utilisateur connecté
-        //console.log("Utilisateur connecté:", users[0]);
-        return { user: users[0] };
-    } catch (error) {
-        console.error("Erreur lors de la connexion:", error.message);
-        return { error: "Erreur interne du serveur" };
+    // Vérifier s'il y a une erreur
+    if (error) {
+      throw error;
     }
+
+    // Vérifier si l'utilisateur existe
+    if (users.length === 0) {
+      console.log("Utilisateur non trouvé");
+      return { error: "Utilisateur non trouvé" };
+    }
+
+    // Si l'utilisateur est trouvé, renvoyer l'utilisateur connecté
+    //console.log("Utilisateur connecté:", users[0]);
+    return { user: users[0] };
+  } catch (error) {
+    console.error("Erreur lors de la connexion:", error.message);
+    return { error: "Erreur interne du serveur" };
+  }
 };
 
-module.exports = { fetchMeublesHomePage, fetchMeubleAdmin, signInUser };
+async function changeState(id) {
+  try {
+    const { error } = await supabase
+      .from("countries")
+      .update({ name: "Australia" })
+      .eq("id", 1);
+  } catch (error) {
+    console.error("Erreur lors de la mise à jour du statut:", error.message);
+    return { error: "Erreur interne du serveur" };
+  }
+}
+
+module.exports = {
+  fetchMeublesHomePage,
+  fetchMeubleAdmin,
+  signInUser,
+  changeState,
+};
