aabe5544395f2e597bc8f5234d029ca99bd0c55b
maxlebro <z.joriot@gamil.com>
2024-03-22 11:39:29 +0100

    Kevin refactoring

--- le_comptoir_front/src/component/productGrids.jsx/productCard.jsx
+++ le_comptoir_front/src/component/productGrids.jsx/productCard.jsx
@@ -9,51 +9,53 @@ import {
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
+export const ProductCard = ({ product }) => {
+  const { id, image, nom, prix, type, description } = product;
 
-export const ProductCard = (props) => {
-  const { product } = props;
-  const { id, image , nom, prix, type, description } = product;
-  
   return (
     <div className="product-card">
-
-
-    <Link
-    to="/product"
-      state={{ product }} 
-    >
-      <Stack id={id}>
-        <Box position="relative">
-          <AspectRatio height={"100%"} width={"100%"} ratio={4 / 4}>
-            <Image
-              src={image}
-              alt={nom}
-              draggable="false"
-              borderRadius={{
-                md: "xl",
-              }}
-            />
-          </AspectRatio>
-        </Box>
-        <Stack>
-          <Stack spacing="1">
-            <Text
-              fontWeight="light"
-              color={useColorModeValue("gray.700", "gray.400")}
-              fontSize={35}
+      <Link to="/product" state={{ product }}>
+        <Stack id={id}>
+          <Box position="relative">
+            <AspectRatio height={"100%"} width={"100%"} ratio={4 / 4}>
+              <Image
+                src={image}
+                alt={nom}
+                draggable="false"
+                borderRadius={{
+                  md: "xl",
+                }}
+              />
+            </AspectRatio>
+          </Box>
+          <Stack>
+            <Stack spacing="1">
+              <Text
+                fontWeight="light"
+                color={useColorModeValue("gray.700", "gray.400")}
+                fontSize={35}
+              >
+                {nom}
+              </Text>
+              <Text fontSize={30} fontWeight="light">
+                {" "}
+                {prix}€{" "}
+              </Text>
+            </Stack>
+          </Stack>
+          <Stack align="center">
+            <Button
+              backgroundColor="#D8C0AA"
+              width="full"
+              color={"white"}
+              fontSize={30}
+              fontWeight="medium"
             >
-              {nom}
-            </Text>
-            <Text fontSize={30} fontWeight="light"> {prix}€ </Text>
+              Acheter
+            </Button>
           </Stack>
         </Stack>
-        <Stack align="center">
-          <Button backgroundColor="#D8C0AA" width="full" color={"white"} fontSize={30} fontWeight="medium" >
-          Acheter
-          </Button>
-        </Stack>
-      </Stack>
-    </Link>
+      </Link>
     </div>
   );
 };
--- le_comptoir_front/src/component/productGrids.jsx/productsList.jsx
+++ le_comptoir_front/src/component/productGrids.jsx/productsList.jsx
@@ -1,56 +1,28 @@
-import { Box } from '@chakra-ui/react'
-import { ProductCard } from './productCard'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
+import { Box } from "@chakra-ui/react";
 
-import "../../App.css"
+import { ProductCard } from "./productCard";
+import { getMeubles } from "../../utils/api";
 
-function ProductList () {
-  const [data, setdata] = useState(null)    // initalise data | setdata est une fonction qui permet de mettre à jour la valeur de data
-  const [selectedTypes, setSelectedTypes] = useState([]);
+import "../../App.css";
 
 
-  const getData = async () => {       //sert à update la valeur de data
-    const res = await fetch("http://localhost:3000/meubles")
-    const meubles = await res.json()
-    setdata(meubles) // ~data = meubles
-  }
-  
-  useEffect(()=>{
-    const fetchMeubles = async () =>{
-      await getData()
-    }
-    fetchMeubles()
-  }, [])
-  
-  const handleCheckboxChange = (type) => {
-    // Mettre à jour les types sélectionnés
-    if (selectedTypes.includes(type)) {
-      setSelectedTypes(selectedTypes.filter((selectedType) => selectedType !== type));
-    } else {
-      setSelectedTypes([...selectedTypes, type]);
-    }
-  };
+function ProductList() {
+  const [meubles, setMeubles] = useState([]); // initalise data | setMeubles est une fonction qui permet de mettre à jour la valeur de data
 
-  const filteredData = data && data.length > 0 ? 
-    selectedTypes.length > 0 ? data.filter((product) => selectedTypes.includes(product.type)) : data 
-    : [];
+  useEffect(() => {
+    getMeubles().then(setMeubles);
+  }, []);
 
-  return(
-
+  return (
     <Box maxW="100%">
-    <Box display={"Flex"} width={"100%"} gap={"4rem"} flexWrap={"wrap"}>
-
-      {/* {filteredData.map((product) => (
-    <ProductCard key={product.id} product={product} />
-  ))} */}
-      {data && data.length > 0 ? 
-      ( data.map((product) => (
-        <ProductCard key={product.id} product={product} />
-        ))
-      ) : "" 
-    }
+      <Box display={"Flex"} width={"100%"} gap={"4rem"} flexWrap={"wrap"}>
+        {meubles.map((meuble) => (
+          <ProductCard key={meuble.id} product={meuble} />
+        ))}
+      </Box>
     </Box>
-  </Box>
-)}
+  );
+}
 
 export default ProductList;
